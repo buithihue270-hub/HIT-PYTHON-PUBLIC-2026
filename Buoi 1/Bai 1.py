f8074998c7577444e90f4263fe1f6f66afc88e93
buithihue270-hub <buithihue270@gmail.com>
2026-07-13 16:20:27 +0700

    BTVN Buoi 1

--- Buoi 1/Bai 1.py
+++ Buoi 1/Bai 1.py
@@ -4,4 +4,3 @@ print("+ Không cần biên dịch thủ công: chạy trực tiếp trên Pytho
 print("+ Thực thi từng dòng: nếu lỗi ở dòng 50, Python vẫn chạy được các dòng từ 1-49 trước khi báo lỗi.")
 print("+ Tính linh hoạt cao: có thể chạy code Python trong môi trường tương tác, gõ lệnh và thấy kết quả ngay lập tức.")
 print("+ Đa nền tảng: cùng một file.py chạy được trên Windows, Linux, macOS miễn là có cài Python interpreter, không cần biên dịch lại.")
-
