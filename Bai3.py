7fd9320467d7aea75922c0b963f2d18440e1d99f
buithihue270-hub <buithihue270@gmail.com>
2026-07-31 22:40:41 +0700

    Nop bai kiem tra giua khoa

---
+++ Bai3.py
@@ -0,0 +1,14 @@
+def tim_vi_tri_tu(chuoi, tu_muc_tieu):
+    danh_sach_tu = chuoi.split()
+    ket_qua = []
+    for vi_tri in range(len(danh_sach_tu)):
+        if danh_sach_tu[vi_tri] == tu_muc_tieu:
+            ket_qua.append(vi_tri)
+    if len(ket_qua) == 0:
+        return -1
+    else:
+        return ket_qua
+chuoi = input("Nhập chuỗi:")
+tu_muc_tieu = input("Nhập từ cần tìm:")
+ket_qua = tim_vi_tri_tu(chuoi, tu_muc_tieu)
+print(ket_qua)
