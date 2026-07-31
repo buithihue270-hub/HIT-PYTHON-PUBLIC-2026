9ac7a7858b80bfca2c81f77a79dc801db3c4fcaf
Bùi Thị Huệ <buithihue270@gmail.com>
2026-07-31 23:25:50 +0700

    Delete Buổi 1 directory

--- Buổi 1/Bài 1.py
+++
@@ -1,7 +0,0 @@
-print("- Python là ngôn ngữ thông dịch")
-print("- Giải thích:")
-print("+ Không cần biên dịch thủ công: chạy trực tiếp trên Python file.py mà không cần các bước compile riêng.")
-print("+ Thực thi từng dòng: nếu lỗi ở dòng 50, Python vẫn chạy được các dòng từ 1-49 trước khi báo lỗi.")
-print("+ Tính linh hoạt cao: có thể chạy code Python trong môi trường tương tác, gõ lệnh và thấy kết quả ngay lập tức.")
-print("+ Đa nền tảng: cùng một file.py chạy được trên Windows, Linux, macOS miễn là có cài Python interpreter, không cần biên dịch lại.")
-
